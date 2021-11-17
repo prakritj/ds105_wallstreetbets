1117a7c77be89a3e0f575537295445a74372b1db
prakritj <p.jayakumar1@lse.ac.uk>
2021-11-17 23:42:20 +0000

    fixed DF issues

--- Data_Collect_3.0.py
+++ Data_Collect_3.0.py
@@ -13,23 +13,14 @@ reddit = praw.Reddit(
     username="ds105_WSB",
 )
 
-
-
-# post = reddit.submission('qskqik')
-# post.comments.replace_more(limit=None)
-# posts_df = posts_df.append({
-#     'id':post.id,
-#     'title':post.title,
-#     'score':post.score,
-#     'top_level_comments': list(post.comments)
-# }, ignore_index=True)
+all_posts_df = pd.DataFrame()
 
 wsb = reddit.subreddit('wallstreetbets')
 for submission in wsb.search('Daily Discussion Thread for', sort='new', time_filter='all', limit=500):
     if 'Daily Discussion Thread for ' in submission.title:
         if 'June' in submission.title or 'July' in submission.title:
-            posts_df = pd.DataFrame()
-            #I need title, id, comments, score, put into posts_df
+            post_df = pd.DataFrame()
+            #I need title, id, comments, score, put into all_posts_df and post_df
             print('Posts: ' + submission.title)
             result = False
             last_exception = None
@@ -45,20 +36,27 @@ for submission in wsb.search('Daily Discussion Thread for', sort='new', time_fil
                     time.sleep(30)
             if not result:
                 raise last_exception
-            posts_df = posts_df.append({
+            comments = list(submission.comments)
+            all_posts_df = all_posts_df.append({
                 'id':submission.id,
                 'title':submission.title,
                 'score':submission.score,
-                'top_level_comments': list(submission.comments)
+                'top_level_comments': comments
+            }, ignore_index=True)
+            post_df = post_df.append({
+                'id':submission.id,
+                'title':submission.title,
+                'score':submission.score,
+                'top_level_comments': comments
             }, ignore_index=True)
             if path.exists('posts.csv'):
-                posts_df.to_csv(r'posts.csv', mode = 'a')
+                post_df.to_csv(r'posts.csv', mode = 'a')
             else:
-                posts_df.to_csv(r'posts.csv', mode = 'w')
+                post_df.to_csv(r'posts.csv', mode = 'w')
 
-for posts in posts_df.itertuples():
-    print('Comments: '+posts.title)
-    for comment in posts.top_level_comments:
+for post in all_posts_df.itertuples():
+    print('Comments: ' + post.title)
+    for comment in post.top_level_comments:
         comments_df = pd.DataFrame()
         comments_df = comments_df.append({
             'id':comment.id,
