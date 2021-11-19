1d6219c9ebfcd39f17be61873ab7d2d246d0e3e0
prakritj <p.jayakumar1@lse.ac.uk>
2021-11-19 15:27:07 +0000

    removed overcomplicated elements

--- Data_Collect_3.0.py
+++ Data_Collect_3.0.py
@@ -1,9 +1,5 @@
 import praw as praw
-import prawcore
 import pandas as pd
-import time
-import os.path as path
-from time import sleep
 
 reddit = praw.Reddit(
     client_id="0GFgVgByJ065sadDY62Q4g",
@@ -13,58 +9,31 @@ reddit = praw.Reddit(
     username="ds105_WSB",
 )
 
-all_posts_df = pd.DataFrame()
-
+posts_df = pd.DataFrame()
 wsb = reddit.subreddit('wallstreetbets')
 for submission in wsb.search('Daily Discussion Thread for', sort='new', time_filter='all', limit=500):
     if 'Daily Discussion Thread for ' in submission.title:
-        if 'June' in submission.title or 'July' in submission.title:
-            post_df = pd.DataFrame()
+        if submission.created_utc: #need to check whether the post is within our two week window
             #I need title, id, comments, score, put into all_posts_df and post_df
             print('Posts: ' + submission.title)
-            result = False
-            last_exception = None
-            timeout = 900 #seconds = 15 minutes
-            time_start = int(time.time())
-            while not result and int(time.time()) < time_start + timeout:
-                try:
-                    submission.comments.replace_more(limit=None)
-                    result = True
-                except prawcore.exceptions.ServerError as e:
-                    #wait for 30 seconds since sending more requests to overloaded server might not be helping
-                    last_exception = e
-                    time.sleep(30)
-            if not result:
-                raise last_exception
-            comments = list(submission.comments)
-            all_posts_df = all_posts_df.append({
+            posts_df = posts_df.append({
                 'id':submission.id,
                 'title':submission.title,
                 'score':submission.score,
-                'top_level_comments': comments
+                'top_level_comments': list(submission.comments)
             }, ignore_index=True)
-            post_df = post_df.append({
-                'id':submission.id,
-                'title':submission.title,
-                'score':submission.score,
-                'top_level_comments': comments
-            }, ignore_index=True)
-            if path.exists('posts.csv'):
-                post_df.to_csv(r'posts.csv', mode = 'a')
-            else:
-                post_df.to_csv(r'posts.csv', mode = 'w')
 
+posts_df.to_csv(r'posts.csv', mode = 'w')
+
+comments_df = pd.DataFrame()
 for post in all_posts_df.itertuples():
     print('Comments: ' + post.title)
     for comment in post.top_level_comments:
-        comments_df = pd.DataFrame()
         comments_df = comments_df.append({
             'id':comment.id,
             'post_id':comment.link_id,
             'body':comment.body,
             'score':comment.score
         }, ignore_index=True)
-        if path.exists('comments.csv'):
-            comments_df.to_csv(r'comments.csv', mode = 'a')
-        else:
-            comments_df.to_csv(r'comments.csv', mode = 'w')
+
+comments_df.to_csv(r'comments.csv', mode = 'w')
